110670548245d26b167856e47422afd1e622a25b
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2023-06-16 16:40:58 -0300

    tratamento para análise longitudinal

--- analise_exploratoria.R
+++ analise_exploratoria.R
@@ -118,34 +118,6 @@ dados$data_fim <- ymd(dados$data_fim)
 dados_longitudinais_trabalho$diferenca_max_meses <- interval(dados_longitudinais_trabalho$data_inscricao,
                                                              dados_longitudinais_trabalho$data_max_referencia) / months(1)
 
-glimpse(dados_longitudinais_trabalho)
-
-dados_longitudinais_trabalho %>%
-  slice_sample(n= 10) %>%
-  readr::write_csv("amostra_dados_longitudinais.csv")
 
 
-# Instale os pacotes necessários
-install.packages("survival")
-install.packages("survminer")
 
-# Carregue os pacotes
-library(survival)
-library(survminer)
-
-# Carregue os dados do arquivo CSV
-
-# Crie um objeto de sobrevivência utilizando a função Surv() do pacote survival
-sobrevivencia <- Surv(time = dados_longitudinais_trabalho$diferenca_max_meses)
-
-# Realize a comparação de grupos usando o teste de log-rank
-comparacao <- survdiff(sobrevivencia ~ tipo_pessoa, data = dados_longitudinais_trabalho)
-
-# Ajuste os modelos de sobrevivência separadamente para cada grupo
-ajuste <- survfit(sobrevivencia ~ tipo_pessoa, data = dados_longitudinais_trabalho)
-
-# Exiba o resultado do teste
-summary(comparacao)
-
-# Plote as curvas de sobrevivência para cada grupo
-ggsurvplot(ajuste, data = dados_longitudinais_trabalho, risk.table = TRUE)
