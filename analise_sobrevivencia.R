c1cfe335df9b6edf233f6da00a7c680d1217ff26
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2023-06-19 17:04:43 -0300

    análise para tipo_situacao_inscricao, situacao_inscricao e recita_principal

--- analise_sobrevivencia.R
+++ analise_sobrevivencia.R
@@ -1,12 +1,13 @@
 ##Análise de sobrevivência
 
 # Instale os pacotes necessários
-install.packages("survival")
-install.packages("survminer")
+#install.packages("survival")
+#install.packages("survminer")
 
 # Carregue os pacotes
 library(survival)
 library(survminer)
+library(questionr)
 
 
 
@@ -50,4 +51,55 @@ ajuste_ajuizado <- survfit(sobrevivencia ~ indicador_ajuizado, data = dados_long
 
 # Plote as curvas de sobrevivência para cada grupo
 ggsurvplot(ajuste_ajuizado, data = dados_longitudinais_trabalho, risk.table = TRUE)
+
+
+############## Análises de sobrevivência com grupos para situacao_inscricao
+
+unique(dados_longitudinais_trabalho$tipo_situacao_inscricao)
+
+# Realize a comparação de grupos usando o teste de log-rank
+comparacao_situacao <- survdiff(sobrevivencia ~ tipo_situacao_inscricao, data = dados_longitudinais_trabalho)
+
+# Exiba o resultado do teste
+summary(comparacao_situacao)
+
+
+# Ajuste os modelos de sobrevivência separadamente para cada grupo
+ajuste_situacao <- survfit(sobrevivencia ~ tipo_situacao_inscricao, data = dados_longitudinais_trabalho)
+
+
+# Plote as curvas de sobrevivência para cada grupo
+ggsurvplot(ajuste_situacao, data = dados_longitudinais_trabalho, risk.table = TRUE)
+
+
+
+########## Situacao_inscricao
+dados_longitudinais_trabalho %>%
+  group_by(situacao_inscricao) %>%
+  summarise(
+    quantidade =  n(),
+    valor_consolidado_total = sum(valor_consolidado),
+    media_valor_consolidado = mean(valor_consolidado),
+    mediana_valor_consolidadeo = median(valor_consolidado)
+  ) %>%
+  slice_max(order_by = valor_consolidado_total, n=10)
+
+
+
+questionr::freq(dados_longitudinais_trabalho$situacao_inscricao, cum = TRUE, sort = "dec", total = TRUE)
+
+########## receita_principal
+
+dados_longitudinais_trabalho %>%
+  group_by(receita_principal) %>%
+  summarise(
+    quantidade =  n(),
+    valor_consolidado_total = sum(valor_consolidado),
+    media_valor_consolidado = mean(valor_consolidado),
+    mediana_valor_consolidado = median(valor_consolidado)
+  ) %>%
+  slice_max(order_by = valor_consolidado_total, n=10)
+
+questionr::freq(dados_longitudinais_trabalho$receita_principal, cum = TRUE, sort = "dec", total = TRUE)
+
 
