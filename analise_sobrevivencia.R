36860a05872c71e5f06e7b290f99f041d1057268
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2023-06-19 14:46:26 -0300

    Análise de sobrevivência com grupo tipo_ajuizado

--- analise_sobrevivencia.R
+++ analise_sobrevivencia.R
@@ -8,19 +8,46 @@ install.packages("survminer")
 library(survival)
 library(survminer)
 
-# Carregue os dados do arquivo CSV
+
+
+dados_longitudinais_trabalho$diferenca_max_meses<- round(dados_longitudinais_trabalho$diferenca_max_meses,0)
+
 
 # Crie um objeto de sobrevivência utilizando a função Surv() do pacote survival
 sobrevivencia <- Surv(time = dados_longitudinais_trabalho$diferenca_max_meses)
 
+
+############## Análises de sobrevivência com grupos para tipo de pessoa
+
 # Realize a comparação de grupos usando o teste de log-rank
 comparacao <- survdiff(sobrevivencia ~ tipo_pessoa, data = dados_longitudinais_trabalho)
-
-# Ajuste os modelos de sobrevivência separadamente para cada grupo
-ajuste <- survfit(sobrevivencia ~ tipo_pessoa, data = dados_longitudinais_trabalho)
 
 # Exiba o resultado do teste
 summary(comparacao)
 
+
+# Ajuste os modelos de sobrevivência separadamente para cada grupo
+ajuste <- survfit(sobrevivencia ~ tipo_pessoa, data = dados_longitudinais_trabalho)
+
+
 # Plote as curvas de sobrevivência para cada grupo
 ggsurvplot(ajuste, data = dados_longitudinais_trabalho, risk.table = TRUE)
+
+
+
+############## Análises de sobrevivência com grupos para tipo ajuizado
+
+# Realize a comparação de grupos usando o teste de log-rank
+comparacao_ajuizado <- survdiff(sobrevivencia ~ indicador_ajuizado, data = dados_longitudinais_trabalho)
+
+# Exiba o resultado do teste
+summary(comparacao_ajuizado)
+
+
+# Ajuste os modelos de sobrevivência separadamente para cada grupo
+ajuste_ajuizado <- survfit(sobrevivencia ~ indicador_ajuizado, data = dados_longitudinais_trabalho)
+
+
+# Plote as curvas de sobrevivência para cada grupo
+ggsurvplot(ajuste_ajuizado, data = dados_longitudinais_trabalho, risk.table = TRUE)
+
