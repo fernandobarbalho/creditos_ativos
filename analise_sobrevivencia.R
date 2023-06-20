e4445fe8446b080b83777d7fb3b1e61c5174e1b9
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2023-06-20 16:45:26 -0300

    Análises de distribuição de valor consolidado

--- analise_sobrevivencia.R
+++ analise_sobrevivencia.R
@@ -20,6 +20,12 @@ sobrevivencia <- Surv(time = dados_longitudinais_trabalho$diferenca_max_meses)
 
 ############## Análises de sobrevivência com grupos para tipo de pessoa
 
+dados_longitudinais_trabalho %>%
+  ggplot() +
+  geom_density(aes(x= valor_consolidado, fill = tipo_pessoa), alpha=0.5) +
+  scale_x_log10()
+
+
 # Realize a comparação de grupos usando o teste de log-rank
 comparacao <- survdiff(sobrevivencia ~ tipo_pessoa, data = dados_longitudinais_trabalho)
 
@@ -36,7 +42,14 @@ ggsurvplot(ajuste, data = dados_longitudinais_trabalho, risk.table = TRUE)
 
 
 
-############## Análises de sobrevivência com grupos para tipo ajuizado
+############## Análises de sobrevivência com grupos para indicador ajuizado
+
+dados_longitudinais_trabalho %>%
+  ggplot() +
+  geom_density(aes(x= valor_consolidado, fill = indicador_ajuizado), alpha=0.5) +
+  scale_x_log10()
+
+
 
 # Realize a comparação de grupos usando o teste de log-rank
 comparacao_ajuizado <- survdiff(sobrevivencia ~ indicador_ajuizado, data = dados_longitudinais_trabalho)
@@ -55,7 +68,15 @@ ggsurvplot(ajuste_ajuizado, data = dados_longitudinais_trabalho, risk.table = TR
 
 ############## Análises de sobrevivência com grupos para tipo_situacao_inscricao
 
+
+
 unique(dados_longitudinais_trabalho$tipo_situacao_inscricao)
+
+dados_longitudinais_trabalho %>%
+  ggplot() +
+  geom_density(aes(x= valor_consolidado, fill = tipo_situacao_inscricao), alpha=0.5) +
+  scale_x_log10()
+
 
 # Realize a comparação de grupos usando o teste de log-rank
 comparacao_situacao <- survdiff(sobrevivencia ~ tipo_situacao_inscricao, data = dados_longitudinais_trabalho)
@@ -96,6 +117,13 @@ top_4_situacao_inscricao<-
   slice_max(order_by = quantidade, n=4))$situacao_inscricao
 
 
+dados_longitudinais_trabalho %>%
+  filter(situacao_inscricao %in% top_4_situacao_inscricao) %>%
+  ggplot() +
+  geom_density(aes(x= valor_consolidado, fill = situacao_inscricao), alpha=0.5) +
+  scale_x_log10()
+
+
 trabalho_situacao_inscricao<-
 dados_longitudinais_trabalho %>%
   filter(situacao_inscricao %in% top_4_situacao_inscricao)
@@ -132,31 +160,47 @@ dados_longitudinais_trabalho %>%
 questionr::freq(dados_longitudinais_trabalho$receita_principal, cum = TRUE, sort = "dec", total = TRUE)
 
 
-top_4_receita_principal<-
+top_8_receita_principal<-
   (dados_longitudinais_trabalho %>%
      group_by(receita_principal) %>%
      summarise(
        quantidade =  n()
      ) %>%
-     slice_max(order_by = quantidade, n=4))$receita_principal
+     slice_max(order_by = quantidade, n=8))$receita_principal
+
+dados_longitudinais_trabalho %>%
+  filter(receita_principal %in% top_8_receita_principal) %>%
+  ggplot() +
+  geom_density(aes(x= valor_consolidado)) +
+  scale_x_log10() +
+  facet_wrap(receita_principal~.)
 
 
 trabalho_receita_principal<-
   dados_longitudinais_trabalho %>%
-  filter(situacao_inscricao %in% top_4_receita_principal)
+  filter(receita_principal %in% top_8_receita_principal)
 
-sobrevivencia_situacao_inscricao <- Surv(time = trabalho_situacao_inscricao$diferenca_max_meses)
+sobrevivencia_receita_principal <- Surv(time = trabalho_receita_principal$diferenca_max_meses)
 
 # Realize a comparação de grupos usando o teste de log-rank
-comparacao_situacao_inscricao <- survdiff(sobrevivencia_situacao_inscricao ~ situacao_inscricao, data = trabalho_situacao_inscricao)
+comparacao_receita_principal <- survdiff(sobrevivencia_receita_principal ~ receita_principal,
+                                          data = trabalho_receita_principal)
 
-# Exiba o resultado do teste
-summary(comparacao_situacao_inscricao)
 
 
 # Ajuste os modelos de sobrevivência separadamente para cada grupo
-ajuste_situacao_inscricao <- survfit(sobrevivencia_situacao_inscricao ~ situacao_inscricao, data = trabalho_situacao_inscricao)
+ajuste_receita_principal <- survfit(sobrevivencia_receita_principal ~ receita_principal, data = trabalho_receita_principal)
 
 
 # Plote as curvas de sobrevivência para cada grupo
-ggsurvplot(ajuste_situacao_inscricao, data = trabalho_situacao_inscricao, risk.table = TRUE)
+ggsurvplot(ajuste_receita_principal, data = trabalho_receita_principal, risk.table = TRUE)
+
+
+##### Valor consolidado
+
+dados_longitudinais_trabalho %>%
+  ggplot() +
+  geom_density(aes(x= valor_consolidado)) +
+  scale_x_log10()
+
+
