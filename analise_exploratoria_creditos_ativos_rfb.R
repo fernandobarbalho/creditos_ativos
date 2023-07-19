5ff075849cbbd1f638a2850d13043e69cc0d99cb
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2023-07-19 11:25:03 -0300

    analisando série temporal rfb

--- analise_exploratoria_creditos_ativos_rfb.R
+++ analise_exploratoria_creditos_ativos_rfb.R
@@ -21,12 +21,33 @@ creditos_ativos %>%
     panel.grid = element_blank(),
 
   ) +
+  ylim(0, 3000) +
   labs(
     title =  "Evolução de valores totais",
     subtitle = "Valores atualizados pelo IPCA",
     y= "Valores totais em R$(bi)",
     x= ""
   )
+
+
+creditos_ativos %>%
+  group_by(data) %>%
+  summarise(valor = sum(vl_total)) %>%
+  ggplot() +
+  geom_line(aes(x=data, y= valor/10^9), color="black") +
+  theme_light()+
+  theme(
+    panel.grid = element_blank(),
+
+  ) +
+  ylim(0, 3000) +
+  labs(
+    title =  "Evolução de valores totais",
+    subtitle = "Valores atualizados pelo IPCA",
+    y= "Valores totais em R$(bi)",
+    x= ""
+  )
+
 
 
 grafico<-
@@ -40,6 +61,7 @@ creditos_ativos %>%
     panel.grid = element_blank(),
 
   ) +
+  ylim(0, 3000) +
   labs(
     title =  "Evolução de valores totais",
     subtitle = "Valores atualizados pelo IPCA",
@@ -48,3 +70,18 @@ creditos_ativos %>%
   )
 
 plotly::ggplotly(grafico)
+
+
+fab_zero<-
+  creditos_ativos %>%
+  filter(data == "2023-02-01",
+         vl_total <= 1)
+
+
+
+creditos_ativos %>%
+  filter(data %in% c(as.Date("2023-02-01") , as.Date("2023-03-01"))) %>%
+  ggplot() +
+  geom_boxplot(aes(x=as.character(data),vl_total)) +
+  scale_y_log10()
+
